03a07fb5279ca2bd3a96648b5fab8b1aa66713aa
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 21:45:11 +0500

    Add src\components\Footer.jsx

---
+++ src\components\Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="has-text-centered">Copyright 2023 Travel Agency. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
